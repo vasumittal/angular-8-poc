94fc8202d9947dd87109a816084f7fe219377109
Vasudeep Mittal <vasudeep.mittal@agreeya.net>
2020-01-14 15:36:00 +0530

    checkin against add user module

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 ﻿import { Component } from '@angular/core';
 import { first } from 'rxjs/operators';
-
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { User } from '@app/_models';
 import { UserService } from '@app/_services';
 
@@ -8,8 +9,14 @@ import { UserService } from '@app/_services';
 export class HomeComponent {
     loading = false;
     users: User[];
+    addUserForm: FormGroup;
+    submitted = false;
+    error = '';
 
-    constructor(private userService: UserService) { }
+    constructor(private userService: UserService,
+        private formBuilder: FormBuilder,
+        private route: ActivatedRoute,
+        private router: Router) { }
 
     ngOnInit() {
         this.loading = true;
@@ -17,5 +24,34 @@ export class HomeComponent {
             this.loading = false;
             this.users = users;
         });
+        this.addUserForm = this.formBuilder.group({
+            username: ['', Validators.required],
+            firstname: ['', Validators.required],
+            lastname: ['', Validators.required],
+        });
+    }
+
+    // convenience getter for easy access to form fields
+    get f() { return this.addUserForm.controls; }
+
+    onSubmit() {
+        this.submitted = true;
+
+        // stop here if form is invalid
+        if (this.addUserForm.invalid) {
+            return;
+        }
+
+        this.loading = true;
+        // this.authenticationService.login(this.f.username.value, this.f.password.value)
+        //     .pipe(first())
+        //     .subscribe(
+        //         data => {
+        //             this.router.navigate([this.returnUrl]);
+        //         },
+        //         error => {
+        //             this.error = error;
+        //             this.loading = false;
+        //         });
     }
 }
